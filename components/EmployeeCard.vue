b920f446dbe747d165342f1c87d72b39e093c20a
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2020-08-09 22:08:02 +0800

    Added EmployeeCard component

---
+++ components/EmployeeCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="rounded-lg overflow-hidden card-container cursor-pointer"
+    @click="showDescription = !showDescription"
+  >
+    <div
+      v-if="!showDescription"
+      class="front bg-cover bg-center flex items-end justify-end w-full h-full"
+      :style="style"
+    >
+      <div class="p-4">
+        <h3 class="font-bold text-lg text-white">{{ name }}</h3>
+        <p class="text-sm text-gray-200">{{ position }}</p>
+      </div>
+    </div>
+    <div v-else class="back flex items-center justify-center w-full h-full bg-gray-900 text-white">
+      {{ jobDescription }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: 'Name',
+    },
+    position: {
+      type: String,
+      default: 'Position',
+    },
+    imgSrc: {
+      type: String,
+      default: 'faces/MjIyNTUuanBn.jpg',
+    },
+    jobDescription: {
+      type: String,
+      default: 'Description',
+    },
+  },
+  data() {
+    return {
+      style: `background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('${this.imgSrc}');`,
+      showDescription: false,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -5,7 +5,7 @@
       title="About Us"
       subtitle="Get to know the team"
     />
-    <div class="content-area divide-y-2">
+    <div class="content-area divide-y-2 w-full">
       <div class="flex flex-col lg:flex-row pb-12 lg:pb-16">
         <template v-for="(section, i) in sections">
           <div :key="i" class="text-center mt-10 lg:w-1/3 lg:px-6 lg:mt-0 first:mt-0">
@@ -18,6 +18,13 @@
         <h1 class="text-2xl font-bold text-navy-500 text-center">
           Meet The Team
         </h1>
+        <div class="mt-6 grid-container lg:mt-12 gap-4">
+          <employee-card />
+          <employee-card />
+          <employee-card />
+          <employee-card />
+          <employee-card />
+        </div>
       </div>
     </div>
   </div>
@@ -26,10 +33,12 @@
 <script lang="ts">
 import Vue from 'vue';
 import HeroSection from '@/components/HeroSection.vue';
+import EmployeeCard from '@/components/EmployeeCard.vue';
 
 export default Vue.extend({
   components: {
     HeroSection,
+    EmployeeCard,
   },
   data() {
     return {
@@ -52,3 +61,22 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: 1fr;
+}
+.grid-container::before {
+  content: '';
+  width: 0;
+  padding-bottom: 100%;
+  grid-row: 1 / 1;
+  grid-column: 1 / 1;
+}
+.grid-container > *:first-child {
+  grid-row: 1 / 1;
+  grid-column: 1 / 1;
+}
+</style>
